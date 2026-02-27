dad87d4e4c104fa37a2d0b7a35f8e167a1f8bb0d
Michał Okulski <michal.okulski@gmail.com>
2026-02-27 22:51:24 +0100

    Add startnet_oldway.cmd for OSD Cloud initialization

---
+++ startnet_oldway.cmd
@@ -0,0 +1,10 @@
+@ECHO OFF
+wpeinit
+cd\
+title OSD 26.2.3.2
+PowerShell -Nol -C Initialize-OSDCloudStartnet
+PowerShell -Nol -C Initialize-OSDCloudStartnetUpdate
+@ECHO OFF
+ECHO Start-OSDCloud
+start /wait PowerShell -NoL -C Start-OSDCloud -OSName 'Windows 11 24H2 x64' -OSLanguage en-us -OSEdition Enterprise -OSActivation Volume -Restart -ZTI
+@ECHO ON
